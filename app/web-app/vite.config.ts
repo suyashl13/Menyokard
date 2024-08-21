778c5a6c17d5dbdba16130e051a406d8f854009c
Suyash Lawand <suyash.lawand@gmail.com>
2024-08-21 19:24:54 +0000

    init: Reinitialized project into Node

---
+++ app/web-app/vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  server: {
+    port: 5173,
+    host: '127.0.0.1'
+  },
+  plugins: [react()],
+})
